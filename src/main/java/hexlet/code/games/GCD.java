e354605f8d08766cac87ef5757d481808e8b82e9
Lisa Sol <vasilinka86@gmail.com>
2022-08-11 14:24:35 -0400

    added AP and Prime games

--- src/main/java/hexlet/code/games/GCD.java
+++ src/main/java/hexlet/code/games/GCD.java
@@ -12,8 +12,8 @@ public class GCD {
     public static void start() {
         String[][] qa = new String[MAX_ROUNDS][MAX_ROUNDS - 1];
         for (int i = 0; i < MAX_ROUNDS; i++) {
-            int num1 = Utils.generateRandomNum(0,RANDOM_UPPERBOUND);
-            int num2 = Utils.generateRandomNum(0,RANDOM_UPPERBOUND);
+            int num1 = Utils.generateRandomNum(1,RANDOM_UPPERBOUND);
+            int num2 = Utils.generateRandomNum(1,RANDOM_UPPERBOUND);
             qa[i][0] = num1 + " " + num2;
             int gcd = 1;
 
